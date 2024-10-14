7b00809725e7f425424ecd219361217a661853a3
h593265 <danield@live.no>
2024-10-14 21:51:20 +0200

    changed ssl

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const postgres = require('postgres');
 require('dotenv').config();
 const sql = postgres({
+  ssl: { rejectUnauthorized: false },
     host                 : process.env.PG_HOST,            
     port                 : process.env.PG_PORT,          
     database             : process.env.PG_DATABASE,            
