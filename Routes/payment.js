09601a002e9de58295e4b621f51a02fadf03700d
h593265 <danield@live.no>
2024-10-17 18:20:06 +0200

    changed payment.js

--- Routes/payment.js
+++ Routes/payment.js
@@ -28,8 +28,8 @@ router.post("/", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cancel",
+      success_url: `${domain}/success`,
+      cancel_url: `${domain}/cancel`,
     });
 
     for (const product of products) {
